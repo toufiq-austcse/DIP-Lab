fe9cac95c44a9782616f99e65192930ae884bff8
toufiq-austcse <toufiq-austcse@gmail.com>
2019-08-05 04:01:12 +0600

    Session 2 added

---
+++ Session2/imagethresholding.m
@@ -0,0 +1,13 @@
+I = imread('https://scontent.fdac5-1.fna.fbcdn.net/v/t1.0-9/65619005_2475230252539555_4619126332008169472_n.jpg?_nc_cat=111&_nc_oc=AQmh8S8X-9qxxsNggf4D3g0cSEehXdgZHQkng6KzeDjhjc4JY_PpPEWuYuCZq9ZkDKQ&_nc_ht=scontent.fdac5-1.fna&oh=da7cf634863baf03ce30203f835e188a&oe=5DDF483C');
+[row,col] = size(I);
+
+for i=1:row
+  for j = 1:col
+    if I(i,j)<110
+      I(i,j) = 0;
+    else
+      I(i,j) = 255;
+    endif
+  endfor
+endfor
+imshow(I);
